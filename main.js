7c8f34e69921b8d19f918b91d55f6a0d69ec1668
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-06-28 10:55:39 +0700

    commit 8

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-const { createApp, ref} = Vue
+const { createApp, ref, computed} = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
-        const image = ref('./assets/images/socks_green.jpg')
+        const brand = ref('SE 331')
+        // const image = ref('./assets/images/socks_green.jpg')
         const description = ref('Made from high-quality combed cotton, these socks are incredibly soft against your skin, providing all-day comfort.')
         const productLink = ref('https://www.camt.cmu.ac.th')
-        const inStock = ref(false)
+        // const inStock = ref(false)
         const onSale = ref(true)
         const inventory = ref(100)
         const details = ref([
             '50% cotton',
-            '30% wool',
-            '20% polyester'
+            ' 30% wool',
+            ' 20% polyester'
         ])
         const sizes = ref([
             'S ','M ','L '
         ])
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
         ])
+        const selectVariant = ref(0)
+        function updateVariant(index){
+            selectVariant.value = index;
+        }
         const cart = ref(0)
         function addToCart(){
             cart.value += 1
         }
-
+        const title = computed(() => {
+            return brand.value + ' ' + product.value
+        })
         function updateImage(variantImage){
             image.value = variantImage
         }
+        const image = computed(() => {
+            return variants.value[selectVariant.value].image
+        })
+        const inStock = computed(() => {
+            return variants.value[selectVariant.value].quantity
+        })
+        // const onSale = computed(() => {
+        //     return variants.value[selectVariant.value].
+        // })
         const toggleStock = () => {
             if (inventory.value > 0) {
                 inventory.value = 0
@@ -37,7 +53,7 @@ createApp({
             }
         }
         return{
-            product, 
+            title,
             description,
             image,
             productLink,
@@ -50,7 +66,8 @@ createApp({
             cart,
             addToCart,
             updateImage,
-            toggleStock
+            toggleStock,
+            updateVariant
         }
     }
 }).mount('#app')
