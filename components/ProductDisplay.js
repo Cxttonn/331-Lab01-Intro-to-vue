5b18947c1313da887288cdfaa848b677e3df8598
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-06-28 13:00:40 +0700

    commit 9

---
+++ components/ProductDisplay.js
@@ -0,0 +1,118 @@
+const productDisplay = {
+
+  template:
+    /*html*/
+    `
+    <div class="product-display">
+        <div class="product-container">
+            <div class="product-image" :class="{ 'out-of-stock-image': !inStock }">
+                <img :src="image">
+            </div>
+        </div>
+        <div class="product-info">
+            <h1><a :href="productLink" target="_blank">{{title}}</a></h1>
+            <p>{{description}}</p>
+            <p v-if="inventory > 10">In Stock</p>
+            <p>Shipping: {{shipping}}</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost out of Stock</p>
+            <p v-else>Out of Stock</p>
+            <p v-if="onSale" class="sale-message">This product is on sale!</p>
+            <p v-else class="sale-message">This product is not on sale.</p>
+            <ul>
+                <li v-for="detail in details">{{detail}}</li>
+            </ul>
+            <ul>
+                <li v-for="size in sizes">{{size}}</li>
+            </ul>
+            <p>{{title + ' ' + " is On Sale" + onSale}}</p>
+            <div v-for="(variant, index) in variants" :key="variant.id"
+    @mouseover = "updateImage(index)" 
+            class="color-circle" :style="{backgroundColor: variant.color}">
+            </div>
+            <button class="button" :disabled="!inStock" @click="addToCart"
+:class="{disabledButton: !inStock}">Add To Cart</button>
+            <button class="button" @click="toggleStock">Stock Status</button>
+
+          </div>
+      </div>
+
+    `,
+  props: {
+    premium: Boolean
+  },
+  setup(props){
+    const shipping = computed(() => {
+        if(props.premium){
+            return 'Free'
+        } else{
+            return 30
+        }
+        
+    })
+        const product = ref('Boots')
+        const brand = ref('SE 331')
+        // const image = ref('./assets/images/socks_green.jpg')
+        const description = ref('Made from high-quality combed cotton, these socks are incredibly soft against your skin, providing all-day comfort.')
+        const productLink = ref('https://www.camt.cmu.ac.th')
+        // const inStock = ref(false)
+        const onSale = ref(true)
+        const inventory = ref(100)
+        const details = ref([
+            ' 50% cotton',
+            ' 30% wool',
+            ' 20% polyester'
+        ])
+        const sizes = ref([
+            'S ','M ','L '
+        ])
+        const variants = ref([
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+        ])
+        const selectVariant = ref(0)
+        function updateVariant(index){
+            selectVariant.value = index;
+        }
+        const cart = ref(0)
+        function addToCart(){
+            cart.value += 1
+        }
+        const title = computed(() => {
+            return brand.value + ' ' + product.value
+        })
+        function updateImage(variantImage){
+            image.value = variantImage
+        }
+        const image = computed(() => {
+            return variants.value[selectVariant.value].image
+        })
+        const inStock = computed(() => {
+            return variants.value[selectVariant.value].quantity
+        })
+        const toggleStock = () => {
+            if (inventory.value > 0) {
+                inventory.value = 0
+            } else {
+                inventory.value = 100
+            }
+        }
+        return{
+            title,
+            description,
+            image,
+            productLink,
+            inStock,
+            inventory,
+            onSale,
+            details,
+            variants,
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            toggleStock,
+            updateVariant,
+            shipping
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,73 +1,88 @@
 const { createApp, ref, computed} = Vue
 
-createApp({
+// createApp({
+//     setup(){
+//         const product = ref('Boots')
+//         const brand = ref('SE 331')
+//         // const image = ref('./assets/images/socks_green.jpg')
+//         const description = ref('Made from high-quality combed cotton, these socks are incredibly soft against your skin, providing all-day comfort.')
+//         const productLink = ref('https://www.camt.cmu.ac.th')
+//         // const inStock = ref(false)
+//         const onSale = ref(true)
+//         const inventory = ref(100)
+//         const details = ref([
+//             '50% cotton',
+//             ' 30% wool',
+//             ' 20% polyester'
+//         ])
+//         const sizes = ref([
+//             'S ','M ','L '
+//         ])
+//         const variants = ref([
+//             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+//             { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+//         ])
+//         const selectVariant = ref(0)
+//         function updateVariant(index){
+//             selectVariant.value = index;
+//         }
+//         const cart = ref(0)
+//         function addToCart(){
+//             cart.value += 1
+//         }
+//         const title = computed(() => {
+//             return brand.value + ' ' + product.value
+//         })
+//         function updateImage(variantImage){
+//             image.value = variantImage
+//         }
+//         const image = computed(() => {
+//             return variants.value[selectVariant.value].image
+//         })
+//         const inStock = computed(() => {
+//             return variants.value[selectVariant.value].quantity
+//         })
+//         // const onSale = computed(() => {
+//         //     return variants.value[selectVariant.value].
+//         // })
+//         const toggleStock = () => {
+//             if (inventory.value > 0) {
+//                 inventory.value = 0
+//             } else {
+//                 inventory.value = 100
+//             }
+//         }
+//         return{
+//             title,
+//             description,
+//             image,
+//             productLink,
+//             inStock,
+//             inventory,
+//             onSale,
+//             details,
+//             variants,
+//             sizes,
+//             cart,
+//             addToCart,
+//             updateImage,
+//             toggleStock,
+//             updateVariant
+//         }
+//     }
+// }).mount('#app')
+
+const app = createApp({
     setup(){
-        const product = ref('Boots')
-        const brand = ref('SE 331')
-        // const image = ref('./assets/images/socks_green.jpg')
-        const description = ref('Made from high-quality combed cotton, these socks are incredibly soft against your skin, providing all-day comfort.')
-        const productLink = ref('https://www.camt.cmu.ac.th')
-        // const inStock = ref(false)
-        const onSale = ref(true)
-        const inventory = ref(100)
-        const details = ref([
-            '50% cotton',
-            ' 30% wool',
-            ' 20% polyester'
-        ])
-        const sizes = ref([
-            'S ','M ','L '
-        ])
-        const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
-        ])
-        const selectVariant = ref(0)
-        function updateVariant(index){
-            selectVariant.value = index;
-        }
         const cart = ref(0)
-        function addToCart(){
-            cart.value += 1
-        }
-        const title = computed(() => {
-            return brand.value + ' ' + product.value
-        })
-        function updateImage(variantImage){
-            image.value = variantImage
-        }
-        const image = computed(() => {
-            return variants.value[selectVariant.value].image
-        })
-        const inStock = computed(() => {
-            return variants.value[selectVariant.value].quantity
-        })
-        // const onSale = computed(() => {
-        //     return variants.value[selectVariant.value].
-        // })
-        const toggleStock = () => {
-            if (inventory.value > 0) {
-                inventory.value = 0
-            } else {
-                inventory.value = 100
-            }
-        }
+        const premium = ref(true)
         return{
-            title,
-            description,
-            image,
-            productLink,
-            inStock,
-            inventory,
-            onSale,
-            details,
-            variants,
-            sizes,
             cart,
-            addToCart,
-            updateImage,
-            toggleStock,
-            updateVariant
+            premium
         }
     }
-}).mount('#app')
+})
+
+app.component('product-display', productDisplay)
+
+app.mount('#app')
