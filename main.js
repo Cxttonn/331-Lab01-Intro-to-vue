cb7113ed38a77d46c1bb55e32e5560b7c9a57767
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-06-28 09:41:53 +0700

    commit 4

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const description = ref('Made from high-quality combed cotton, these socks are incredibly soft against your skin, providing all-day comfort.')
         const productLink = ref('https://www.camt.cmu.ac.th')
+        const inStock = ref(true)
+        const onSale = ref(true)
+        const inventory = ref(100)
         return{
             product, 
             description,
             image,
-            productLink
+            productLink,
+            inStock,
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
