14f9f32cd11c48de929092578229c2277cc741e4
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-06-28 09:30:23 +0700

    commit 3

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ const { createApp, ref} = Vue
 createApp({
     setup(){
         const product = ref('Boots')
+        const image = ref('./assets/images/socks_green.jpg')
         const description = ref('Made from high-quality combed cotton, these socks are incredibly soft against your skin, providing all-day comfort.')
+        const productLink = ref('https://www.camt.cmu.ac.th')
         return{
-            product, description
+            product, 
+            description,
+            image,
+            productLink
         }
     }
 }).mount('#app')
