68242f5e5ec165bb29cb741955123a25f91cbc9b
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-06-28 09:54:13 +0700

    commit 5

--- main.js
+++ main.js
@@ -9,6 +9,15 @@ createApp({
         const inStock = ref(true)
         const onSale = ref(true)
         const inventory = ref(100)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            {id: 2234, color:'green'},
+            {id: 2235, color:'blue'}
+        ])
         return{
             product, 
             description,
@@ -16,7 +25,9 @@ createApp({
             productLink,
             inStock,
             inventory,
-            onSale
+            onSale,
+            details,
+            variants
         }
     }
 }).mount('#app')
